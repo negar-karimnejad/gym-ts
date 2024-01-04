dcb8b3e2e5c4139a880bdbeef4538fb0ec46e605
Negar <negar.karimnejad@gmail.com>
2024-01-04 17:51:27 +0330

    coding

--- src/scenes/ourClasses/index.tsx
+++ src/scenes/ourClasses/index.tsx
@@ -5,7 +5,7 @@ function OurClasses() {
     <div className="mt-16 bg-primary-100 py-16">
       <div className="mx-auto w-5/6 md:my-5">
         <h2 className="font-montserrat text-xl font-extrabold">Our Classes</h2>
-        <p className="w-3/5 py-5">
+        <p className="py-5 md:w-3/5">
           Fringilla a sed at suspendisse ut enim volutpat. Rhoncus vel est
           tellus quam porttitor. Mauris velit euismod elementum arcu neque
           facilisi. Amet semper tortor facilisis metus nibh. Rhoncus sit enim
